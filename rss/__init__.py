6e5513ed52e9f3a026962ed8208ad441c065f02e
gk11 <me@51t.ru>
2017-12-01 12:52:55 +1000

    rm pyrss license

--- rss/__init__.py
+++ rss/__init__.py
@@ -1,50 +1,32 @@
 import api, api.sx as sx, datetime
+from api.bottle import template
 import conf
+
+def msglst(l,num):
+    out = []
+    for x in reversed(l[-num:]):
+        n = api.get_msg(x)
+        n.update({'url': '%s/msg/%s#go_%s' % (conf.URL,n.topicid,n.msgid),'pubDate': datetime.datetime.fromtimestamp(n.date)})
+        out.append(n)
+    return out
+
 
 def gen_topic(topicid,num=30):
     comms = api.lst('topic/%s' % topicid)
-    ea = comms[2]
-    tdesc = comms[1]
-    comms = comms[6:]
-    msgs = [api.get_msg(n) for n in reversed(comms[-num:])]
-    items = [PyRSS2Gen.RSSItem(
-        title=n.title,description=sx.rend(n.txt.decode('utf-8')),
-        link='%s/msg/%s#go_%s' % (conf.URL,topicid,n.msgid),
-        guid='%s/msg/%s#go_%s' % (conf.URL,topicid,n.msgid),
-        author=n.who,
-        pubDate=datetime.datetime.fromtimestamp(n.date)
-    ) for n in msgs ]
-    rssout = PyRSS2Gen.RSS2(title=ea,link='%s/msg/%s' % (conf.URL,topicid),description=tdesc,
-    lastBuildDate=datetime.datetime.now(),items=items)
-    return rssout.to_xml('utf-8')
+    ea, desc = comms[2], comms[1]
+    return template('rss/rss.tpl',msgs=msglst(comms[6:],num),title=ea,link='%s/msg/%s' % (conf.URL,topicid),desc=desc)
+
 
 def gen_blog(ea,num=30):
-    comms = api.get_ea(ea)
-    msgs = [api.get_msg(n) for n in reversed(comms[-num:])]
-    items = [PyRSS2Gen.RSSItem(
-        title=n.title,description=sx.rend(n.txt.decode('utf-8')),
-        link='%s/msg/%s' % (conf.URL,n.msgid),
-        guid='%s/msg/%s' % (conf.URL,n.msgid),
-        author=n.who,
-        pubDate=datetime.datetime.fromtimestamp(n.date)
-    ) for n in msgs ]
-    rssout = PyRSS2Gen.RSS2(title=ea,link='%s/blog/%s' % (conf.URL,ea),description='%s: %s' % (ea,api.echo_desc(ea)),
-    lastBuildDate=datetime.datetime.now(),items=items)
-    return rssout.to_xml('utf-8')
+    topics = set(api.lst('topic.list'))
+    comms = [x for x in api.get_ea(ea) if x in topics]
+    return template('rss/rss.tpl',msgs=msglst(comms,num),title=ea,link='%s/blog/%s' % (conf.URL,ea),desc='%s: %s' % (ea,api.echo_desc(ea)))
+
 
 def gen_all(ea,num=30):
     comms = api.get_ea(ea)
-    msgs = [api.get_msg(n) for n in reversed(comms[-num:])]
-    items = [PyRSS2Gen.RSSItem(
-        title=n.title,description=sx.rend(n.txt.decode('utf-8')),
-        link='%s/msg/%s#go_%s' % (conf.URL,n.topicid,n.msgid),
-        guid='%s/msg/%s#go_%s' % (conf.URL,n.topicid,n.msgid),
-        author=n.who,
-        pubDate=datetime.datetime.fromtimestamp(n.date)
-    ) for n in msgs ]
-    rssout = PyRSS2Gen.RSS2(title=ea,link='%s/blog/%s' % (conf.URL,ea),description='%s: %s' % (ea,api.echo_desc(ea)),
-    lastBuildDate=datetime.datetime.now(),items=items)
-    return rssout.to_xml('utf-8')
+    return template('rss/rss.tpl',msgs=msglst(comms,num),title=ea,link='%s/blog/%s' % (conf.URL,ea),desc='%s: %s' % (ea,api.echo_desc(ea)))
+
 
 def lastcomm(ea,num,userdir=''):
     if ea:
